f4c8d2b385e1a43ebfe91bb15e843c19bcb20b67
xepiqq <jaydencrowther@gmail.com>
2024-11-27 17:09:53 -0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
-<div class="flex h-screen flex-col justify-center px-6 py-12 lg:px-8 items-center">
-	<div class="sm:mx-auto sm:w-full sm:max-w-sm">
+<div class="flex h-screen flex-col justify-center px-4 py-12 lg:px-8 items-center">
+	<div class="w-full sm:mx-auto sm:max-w-sm">
 	  <img
 		class="mx-auto h-10 w-auto"
 		src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
 		alt="Your Company"
 	  />
 	  <h2 class="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
-		Rasfiber Payroll Admin
+		Rasfiber Payroll
 	  </h2>
 	</div>
   
-	<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+	<div class="mt-10 w-full sm:mx-auto sm:max-w-sm">
 	  <form class="space-y-6" action="?/login" method="POST">
 		<div>
 		  <label for="email" class="block text-sm font-medium text-gray-900">
@@ -60,7 +60,6 @@
 		  </button>
 		</div>
 	  </form>
-  
 	</div>
   </div>
   
